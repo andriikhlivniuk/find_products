43c36a68aff5ef06112f0f70f125620531e1dc62
Andrii Khlivniuk <andresplattform@gmail.com>
2024-12-15 13:12:12 +0100

    fixes

--- main.py
+++ main.py
@@ -7,48 +7,43 @@ import json
 from urllib.parse import urlparse, urlunparse
 from playwright_stealth import stealth_sync
 
+def best_match_helper(my_products, store_products, index, row, store_name):
+    best_match = None
+    found_url = None
+    found_price = None
+    highest_score = 70
+    if store_name == 'atb':
+        highest_score = 55
+
+    words = row['Назва Товару'].split()
+    brand = words[1] if len(words) > 1 else None
+
+    for product, price_and_url in store_products.items():
+
+        if brand and brand.lower() not in product.lower():
+            continue
+        
+        score = fuzz.ratio(row['Назва Товару'], product)
+        
+        if score > highest_score:
+            highest_score = score
+            best_match = product
+            found_url = price_and_url['url']
+            found_price = price_and_url['price']
+
+    if best_match:
+        my_products.loc[index, f'{store_name}_url'] = found_url
+        my_products.loc[index, f'{store_name}_price'] = found_price
 
 
 def find_best_match(my_products, atb_products = {}, silpo_products = {}, varus_products = {}):
-
+    
     for index, row in my_products.iterrows():
-        best_match = None
-        found_url = None
-        found_price = None
-        highest_score = 70
 
-        # Silpo 
-        for product, price_and_url in silpo_products.items():
-            score = fuzz.ratio(row['Назва Товару'], product)
-            
-            if score > highest_score:
-                highest_score = score
-                best_match = product
-                found_url = price_and_url['url']
-                found_price = price_and_url['price']
-
-        if best_match:
-            my_products.loc[index, 'silpo_url'] = found_url
-            my_products.loc[index, 'silpo_price'] = found_price
+        best_match_helper(my_products, silpo_products, index, row, "silpo")
+        best_match_helper(my_products, atb_products, index, row, "atb")
+        best_match_helper(my_products, varus_products, index, row, "varus")
         
-        # Varus
-        best_match = None
-        found_url = None
-        found_price = None
-        highest_score = 70
-
-        for product, price_and_url in varus_products.items():
-            score = fuzz.ratio(row['Назва Товару'], product)
-            
-            if score > highest_score:
-                highest_score = score
-                best_match = product
-                found_url = price_and_url['url']
-                found_price = price_and_url['price']
-
-        if best_match:
-            my_products.loc[index, 'varus_url'] = found_url
-            my_products.loc[index, 'varus_price'] = found_price
 
     return my_products
 
@@ -146,6 +141,8 @@ def scrap_varus_products_from_page(soup, product_dict):
             'url': product_url,
             'price': special_price
         }
+        product_url = None
+        special_price = None
 
 
 def get_varus_products(url):
@@ -303,11 +300,13 @@ def main ():
 
     # get_and_match_masterzoo_products(masterzoo_url, df_porducts)
 
-    #atb_products = get_atb_products(atb_url)
+    # atb_products = get_atb_products(atb_url)
     silpo_products = get_silpo_products(silpo_url)
-    # varus_products = get_varus_products(varus_url)
+    #varus_products = get_varus_products(varus_url)
+    # Find the best matches
 
-    # # Find the best matches
+    # matches = find_best_match(df_porducts, atb_products=atb_products,silpo_products=silpo_products,varus_products=varus_products)
+    
     matches = find_best_match(df_porducts, silpo_products=silpo_products)
 
     output_file = 'output.csv'
